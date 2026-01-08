e40622a5b7ec0483db5e7d0c406a1ab3bfa6428a
Aarti <94497910+coderanu2021@users.noreply.github.com>
2026-01-08 16:40:55 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-       
+        // Force HTTPS
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
     
     }
 }
